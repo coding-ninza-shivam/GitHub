ecdc6e8c0abcd15a889a88abf2d1550427ab52a3
Shivam Kumar <9.9.2006.shivam.kumar@gmail.com>
2025-05-27 08:40:44 +0530

    route

--- Coding/Python/FlaskPY/app.py
+++ Coding/Python/FlaskPY/app.py
@@ -8,7 +8,12 @@ app=Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return"Welcome to flask"
+    return"Welcome to flask."
+
+@app.route("/index")
+def index():
+    return"Welcome to the index page."
+
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
