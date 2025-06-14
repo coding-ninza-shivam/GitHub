b70a25d5e4431e1c14aa37531f3e7ba51084f443
Shivam Kumar <9.9.2006.shivam.kumar@gmail.com>
2025-06-14 23:30:44 +0530

    flask get and post

--- Coding/Python/FlaskPY/app.py
+++ Coding/Python/FlaskPY/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+
+
+from flask import Flask,render_template,request
 '''
 It creates an instance of the Flask class,
 which will be your WSGI(Web Server Gateway Interface) application.
@@ -8,11 +10,24 @@ app=Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return"Welcome to flask."
+    return"<html><h1>Welcome to this page</h1></html>"
 
-@app.route("/index")
+@app.route("/index",methods=['GET'])
 def index():
-    return"Welcome to the index page."
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/form",methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}!'
+    return render_template("form.html")
+
+
 
 
 if __name__=="__main__":
--- Coding/Python/FlaskPY/main.py
+++ Coding/Python/FlaskPY/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 '''
 It creates an instance of the Flask class,
 which will be your WSGI(Web Server Gateway Interface) application.
@@ -8,16 +8,11 @@ app=Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return"<html><h1>Welcome to this page</h1></html>"
+    return"Welcome to Flask.This is home page."
 
 @app.route("/index")
 def index():
-    return render_template('index.html')
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
+    return"Welcome to the index page."
 
 
 if __name__=="__main__":
