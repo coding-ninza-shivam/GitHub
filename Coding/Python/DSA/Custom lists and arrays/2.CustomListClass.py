72db56a4bada966a374eac616072db7373ddb439
Shivam Kumar <9.9.2006.shivam.kumar@gmail.com>
2025-06-21 10:00:42 +0530

    DSA
    custom list class

--- Coding/Python/DSA/Custom lists and arrays/2.CustomListClass.py
+++ Coding/Python/DSA/Custom lists and arrays/2.CustomListClass.py
@@ -29,7 +29,7 @@ class CustomList:
         def __len__(self):
             return self.size
         
-        def __str__(self):
+        def __str__(self): # Ex: print(mylist)
             output=""
             for i in range(self.size):
                 output = output + str(self.array[i]) + ","
@@ -42,6 +42,34 @@ class CustomList:
             self.size=self.size-1
             return poppeditem
         
+        def __getitem__(self,index):  # EX: myitem[1]
+            if (index>=0 and index<self.size):
+                return self.array[index]
+            else:
+                return 'IndexError: index out of range '
+             
+        def clear(self):  # It does not removes the elements, it reinitialises the string.
+            self.size=0
+
+        def insert(self,position,element): # Position will be inside only.
+            if (self.capacity==self.size):
+                self.__resize(2*self.capacity)
+            for index in range(self.size,position,-1):
+                self.array[index]=self.array[index-1]
+            self.array[position]=element
+            self.size+=1
+
+    def remove(self,element):
+        found=False
+        for position  in range(0,self.size):
+            if self.array[position]==element:
+                found=True 
+                break
+        if not found:
+            raise ValueError("list.remove(x): x not in list")
+        for index in range(position, self.size - 1):
+            self.array[index]=self.array[index+1]
+        self.size-=1
 
 myList=CustomList()
 myList.append(1)
